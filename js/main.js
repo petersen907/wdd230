be8e1aa0b6208e48245578a6dc9c3bc055e092dd
petersen907 <55543355+petersen907@users.noreply.github.com>
2022-09-22 23:58:41 -0600

    removed js problem

--- js/main.js
+++ js/main.js
@@ -2,4 +2,4 @@ const today = new Date();
         document.querySelector("footer div span").textContent = today.getFullYear();
         // document.getElementById("lastupdatedate").textContent = document.lastModified;
         const lastModified = new Date(document.lastModified);
-        document.getElementById("lastupdatedate").textContent = `${lastModified.getMonth()+1}/${lastModified.getDate()}/${lastModified.getFullYear()}/${lastModified.getTime()}`;
+        document.getElementById("lastupdatedate").textContent = `${lastModified.getMonth()+1}/${lastModified.getDate()}/${lastModified.getFullYear()}`;
