fd934dc4d394515edbb63239285a3534bf9c0509
petersen907 <55543355+petersen907@users.noreply.github.com>
2022-10-13 19:23:22 -0600

    Book of Mormon page link added

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -1,17 +1,31 @@
-//
+//hamburger menu
 function toggleMenu(){
     document.getElementById("navbar").classList.toggle("open");
     document.getElementById("hamburger").classList.toggle("open");
 }
 const mybutton = document.getElementById("hamburger");
 mybutton.onclick = toggleMenu;
-    
-//footer js
+
+//
+const cday = new Date();
+//const today = Date.now();
+
+// if (cday.getDay() == 4){
+//     document.querySelector("header p");
+//     pthing.style.display='block';
+//     pthing.style.fontSize= "20pt";
+// }
+   
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const today = new Date();
+let day = days[today.getDay()] + ", " + (today.getDate()) + " " + (months[today.getMonth()]) + " " + (today.getFullYear());
+//header
+        document.querySelector("header div span").textContent = day;   
         document.querySelector("footer div span").textContent = today.getFullYear();
         // document.getElementById("lastupdatedate").textContent = document.lastModified;
         const lastModified = new Date(document.lastModified);
-        document.getElementById("lastupdatedate").textContent = `${lastModified.getMonth()+1}/${lastModified.getDate()}/${lastModified.getFullYear()}`;
+        document.getElementById("lastupdatedate").textContent = document.lastModified;
 /*
 const date = new Date();
 document.querySelector("currentdate").textContent = date.getDate();
