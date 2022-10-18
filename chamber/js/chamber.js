6b7b17f4854011af563935c85067b9219cd4cc15
petersen907 <55543355+petersen907@users.noreply.github.com>
2022-10-18 14:20:14 -0600

    add content to divs

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -36,7 +36,7 @@ const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 	now
 );
-datefield.innerHTML = `<em>${fulldate}</em>`;
+// datefield.innerHTML = `<em>${fulldate}</em>`;
 /*const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation')
 
