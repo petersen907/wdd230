50b6bee272f243af032b317e6baf6eae64aa1f8a
petersen907 <55543355+petersen907@users.noreply.github.com>
2022-10-18 18:33:21 -0600

    fixed header and hero image

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # wdd230
+
+<nav>
+<picture>
+<source media="(min-width: 600px) and (max-width:800px)" srcset="image/goeshere.jpg">
---
+++ chamber/js/windchill.js
@@ -0,0 +1,22 @@
+
+function setwindchill(temp, windspeed){
+// References to DOM elements
+let tempobj = document.querySelector("#temperature");
+let windspeedobj = document.querySelector("#windspeed");
+let windchillobj = document.querySelector("#windchill");
+
+// Calculate windspeed if necessary
+let windchillmsg = "N/A";
+
+if (temp <= 50 && windspeed > 3){
+    let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
+    windchillmsg = `${chill}&deg; F`;
+}
+
+// Populate the DOM stuff
+tempobj.textContent = temp;
+windspeedobj.textContent = windspeed;
+windchillobj.innerHTML = windchillmsg;
+}
+
+setwindchill(39, 10);
